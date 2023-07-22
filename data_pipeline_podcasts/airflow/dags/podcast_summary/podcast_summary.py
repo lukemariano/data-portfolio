5ba270a29d6d642c75f18d2f7caa5c35efc26587
Lucas Mariano <lucas.svmariano@gmail.com>
2023-07-21 22:55:32 -0300

    finalização da pipeline de dados para extrair dados de um podcast

--- data_pipeline_podcasts/airflow/dags/podcast_summary/podcast_summary.py
+++ data_pipeline_podcasts/airflow/dags/podcast_summary/podcast_summary.py
@@ -1,47 +1,47 @@
-import xmltodict
-import requests
 import os
-
+import json
+import requests
+import xmltodict
 import pandas as pd
-import pendulum
 
 from airflow.decorators import dag, task
+import pendulum
 from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.sqlite.hooks.sqlite import SqliteHook
 
-
 PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
+EPISODE_FOLDER = "audios_content"
 
 @dag(
     dag_id='podcast_summary',
     schedule_interval="@daily",
     start_date=pendulum.datetime(2023, 7, 20),
     catchup=False,
-    
 )
 def podcast_summary():
-
     create_database = SqliteOperator(
-        task_id="create_table_sqlite",
+        task_id='create_table_sqlite',
         sql=r"""
         CREATE TABLE IF NOT EXISTS episodes (
             link TEXT PRIMARY KEY,
             title TEXT,
             filename TEXT,
             published TEXT,
-            description TEXT
-        )
+            description TEXT,
+            transcript TEXT
+        );
         """,
         sqlite_conn_id="podcasts"
     )
+
 
     @task()
     def get_episodes():
         data = requests.get(PODCAST_URL)
         feed = xmltodict.parse(data.text)
-        episodes = feed["rss"]["channel"]["item"] # contém cada podcast individual do scraping
+        episodes = feed["rss"]["channel"]["item"] # contém cada podcast individual
 
-        print(f"Found {len(episodes)} episodes.")
+        print(f"{len(episodes)} Episódios foram encontrados.")
 
         return episodes
     
@@ -49,21 +49,22 @@ def podcast_summary():
     @task()
     def load_episodes(episodes):
         hook = SqliteHook(sqlite_conn_id="podcasts")
-        stored = hook.get_pandas_df("SELECT * FROM episodes;")
+        stored_episodes = hook.get_pandas_df("SELECT * FROM episodes;")
         new_episodes = []
 
         for episode in episodes:
-            if episode["link"] not in stored["link"].values:
+            if episode["link"] not in stored_episodes["link"].values:
                 filename = f"{episode['link'].split('/')[-1]}.mp3"
                 new_episodes.append([episode["link"], episode["title"], episode["pubDate"], episode["description"], filename])
-        hook.insert_rows(table="episodes", rows=new_episodes, target_fields=["link", "title", "published", "description", "filename"])
 
-    
+        hook.insert_rows(table='episodes', rows=new_episodes, target_fields=["link", "title", "published", "description", "filename"])
+
+
     @task()
     def download_episodes(episodes):
         for episode in episodes:
             filename = f"{episode['link'].split('/')[-1]}.mp3"
-            audio_path = os.path.join("episodes", filename)
+            audio_path = os.path.join(EPISODE_FOLDER, filename)
             
             if not os.path.exists(audio_path):
                 print(f"Baixando {filename}")
@@ -71,11 +72,13 @@ def podcast_summary():
                 with open(audio_path, "wb+") as f:
                     f.write(audio.content)
 
-    
 
     podcast_episodes = get_episodes()
     store_data = load_episodes(podcast_episodes)
-    
-    create_database >> podcast_episodes >> store_data
+    get_audios = download_episodes(podcast_episodes)
+
+
+    create_database >> podcast_episodes >> [store_data, get_audios]
+
 
 summary = podcast_summary()
